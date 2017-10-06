075a4f30e650c55e3540fe114a6a768335ac5cb7
Greg Lechkun <32270153+glechkun@users.noreply.github.com>
2017-10-05 21:43:57 -0400

    Add files via upload
    Multithreading experiment

---
+++ BrainPadApplication_MultiThreading_experiment/BrainPadApplication_MultiThreading_experiment/Program.cs
@@ -0,0 +1,39 @@
+﻿using GHIElectronics.TinyCLR.BrainPad;
+using System;
+using System.Threading;
+
+namespace BrainPadApplication_MultiThreading_experiment
+{
+    class Program
+    {
+        public void BrainPadSetup()
+        {
+            //Put your setup code here. It runs once when the BrainPad starts up.
+
+            BrainPad.Display.DrawTextAndShowOnScreen(0,0,"Multi");
+        }
+
+        public void BrainPadLoop()
+        {
+            Thread light = new Thread(Second_thread);
+            light.Start();
+
+
+
+            BrainPad.Display.DrawTextAndShowOnScreen(0, 0, "Line 1");
+            BrainPad.Wait.Seconds(1);
+            BrainPad.Display.DrawTextAndShowOnScreen(15, 15, "Line 2");
+            BrainPad.Wait.Seconds(1);
+            BrainPad.Display.DrawTextAndShowOnScreen(30, 30, "Line 3");
+            BrainPad.Wait.Seconds(1);
+        }
+
+        public void Second_thread()
+        {
+            BrainPad.LightBulb.TurnWhite();
+            BrainPad.Wait.Seconds(1);
+            BrainPad.LightBulb.TurnOff();
+            BrainPad.Wait.Seconds(1);
+        }
+    }
+}

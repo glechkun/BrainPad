e8d46eab558d08e072217cb3fe497b466f4bfd5e
Greg Lechkun <32270153+glechkun@users.noreply.github.com>
2017-10-06 09:34:51 -0400

    Add files via upload
    Updates to Multithread to prevent memory crash, and new small fun program

---
+++ BrainPadApplication_MultiThreading_experiment/BrainPadApplication_Annoy_Mom/BrainPadApplication_Annoy_Mom/Program.cs
@@ -0,0 +1,36 @@
+﻿using GHIElectronics.TinyCLR.BrainPad;
+
+namespace BrainPadApplication_Annoy_Mom
+{
+    class Program
+    {
+        public void BrainPadSetup()
+        {
+            BrainPad.Display.DrawTextAndShowOnScreen(0, 0, "Annoy!");
+        }
+
+        public void BrainPadLoop()
+        {
+            //BrainPad.Wait.Seconds(2);
+
+            double X = 200;
+
+            while (X < 2000)
+            {
+                BrainPad.Display.DrawNumberAndShowOnScreen(0, 0, X);
+                BrainPad.Buzzer.StartBuzzing(X);
+
+                X+= 10;
+            }
+
+            while (X > 200)
+            {
+                BrainPad.Display.DrawNumberAndShowOnScreen(0, 0, X);
+                BrainPad.Buzzer.StartBuzzing(X);
+
+                X-= 10;
+            }
+            
+        }
+    }
+}
---
+++ BrainPadApplication_MultiThreading_experiment/BrainPadApplication_MultiThreading_experiment/BrainPadApplication_MultiThreading_experiment/Program.cs
@@ -0,0 +1,41 @@
+﻿using GHIElectronics.TinyCLR.BrainPad;
+using System;
+using System.Threading;
+
+namespace BrainPadApplication_MultiThreading_experiment
+{
+    class Program
+    {
+        public void BrainPadSetup()
+        {
+            //Put your setup code here. It runs once when the BrainPad starts up.
+
+            BrainPad.Display.DrawTextAndShowOnScreen(0,0,"Multi");
+        }
+
+        public void BrainPadLoop()
+        {
+            Thread light = new Thread(Second_thread);
+            light.Start();
+
+
+            while (true) //added this loop to prevent memory from filling up (aka FORK BOMB)
+            {
+                BrainPad.Display.DrawTextAndShowOnScreen(0, 0, "Line 1");
+                BrainPad.Wait.Seconds(1);
+                BrainPad.Display.DrawTextAndShowOnScreen(15, 15, "Line 2");
+                BrainPad.Wait.Seconds(1);
+                BrainPad.Display.DrawTextAndShowOnScreen(30, 30, "Line 3");
+                BrainPad.Wait.Seconds(1);
+            }
+        }
+
+        public void Second_thread()
+        {
+            BrainPad.LightBulb.TurnWhite();
+            BrainPad.Wait.Seconds(1);
+            BrainPad.LightBulb.TurnOff();
+            BrainPad.Wait.Seconds(1);
+        }
+    }
+}
